db101de7c727d16199d9cb581db1688d77eb7099
Luke Markham <lukeeliotmarkham2@hotmail.com>
2019-08-08 17:06:23 +0700

    Created spa functionality with react router. Added shop page which renders data of collections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
-
-import Homepage from "./pages/homepage/homepage.component";
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
     </div>
   );
 }
